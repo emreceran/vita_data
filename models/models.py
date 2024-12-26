75f93cab7bbe0a2d7a37c84ecb27d8ef0bd8ce63
emreceran <emrecrn@gmail.com>
2024-12-26 17:00:47 +0300

    1

--- models/models.py
+++ models/models.py
@@ -31,8 +31,3 @@ class ProductPublicCategory(models.Model):
 
 
 
-class CustomResUsers(models.Model):
-    _inherit = 'res.users'
-
-    profilePage = fields.Char(string="Profil", default="-")
-    linkedin = fields.Char(string="Linkedin", default="-")
