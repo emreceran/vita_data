3e38ca013a3dcd0d680c4c0c095ea73768706273
emreceran <emrecrn@gmail.com>
2024-12-26 13:50:37 +0300

    res düzenlemesi

--- models/models.py
+++ models/models.py
@@ -34,5 +34,5 @@ class ProductPublicCategory(models.Model):
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    profile = fields.Char(string="Profil")
-    linkedin = fields.Char(string="Linkedin")
+    profile = fields.Char(string="Profil", default=" ")
+    linkedin = fields.Char(string="Linkedin", default=" ")
